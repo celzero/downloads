7808b87856b6a8181da13b0a75cc8f165b5bdfa2
Murtaza Aliakbar <murtaza@live.in>
2026-02-27 02:02:37 +0530

    geoip: update

--- src/cfg.js
+++ src/cfg.js
@@ -78,10 +78,10 @@ export function wrap(env) {
     console.debug("cfg: warp?", w, "vcode", v, "r2?", r2, "workers?", cf);
   }
 
-  // values from 17 May 2023
+  // values from 27 Feb 2026
   if (cenv.WARP_ACTIVE == null) cenv.WARP_ACTIVE = "true";
   if (cenv.LATEST_VCODE == null) cenv.LATEST_VCODE = "53";
-  if (cenv.GEOIP_TSTAMP == null) cenv.GEOIP_TSTAMP = "1667349639157";
+  if (cenv.GEOIP_TSTAMP == null) cenv.GEOIP_TSTAMP = "1772137832624";
   if (cenv.STORE_URL == null) cenv.STORE_URL = "https://dist.rethinkdns.com/";
   if (cenv.R2_STORE_URL == null)
     cenv.R2_STORE_URL = "https://cfstore.rethinkdns.com/";
